f7fa1734fdd8b68fa443db82df014cddaf0597c8
Cyrille Mancini <cyrille.mancini@devologeek.net>
2023-07-25 14:15:19 +0200

    Workaround Found

---
+++ CustomerProjet/SimpleColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace SharedComponent;
+
+public class SimpleColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        int batteryLevel = (int)value;
+
+        if (batteryLevel < 25)
+            return Colors.Red;
+        else if (batteryLevel < 50)
+            return Colors.Orange;
+        else if (batteryLevel < 75)
+            return Colors.Yellow;
+        else
+            return Colors.GreenYellow;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
